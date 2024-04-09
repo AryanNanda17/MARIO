1f10472a7d3cfc84b46c2b2864ef3a7b748beda1
Aryan <nandaaryan823@gmail.com>
2024-04-09 11:15:13 +0530

    Homebrew doesn't include pip explicitly, so it was taken out.

--- mario_install.sh
+++ mario_install.sh
@@ -30,7 +30,7 @@ else
                 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
                 echo "homebrew installed successfully"
             fi
-            brew install git cmake ninja dfu-util python3 pip 
+            brew install git cmake ninja dfu-util python3  
             ;;
         *)          
             echo "Not supported: ${unameOut}"
